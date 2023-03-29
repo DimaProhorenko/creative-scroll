3ab939b6d3ac06a301ffcab1e65f228dcfe80ee0
DimaProhorenko <dima.study.web@gmail.com>
2023-03-29 14:44:11 +0300

    scroll animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,62 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-const scroller = ScrollSmoother.create({
-    wrapper: '.scroll-wrapper',
-    content: '.scroll-content',
-    smooth: 1.5,
-    effects: true,
-})
+const originalSpeeds = [];
+const targets = gsap.utils.toArray('.reset-on-mobile');
+targets.forEach(t => originalSpeeds.push(t.getAttribute('data-speed')))
+const media = '(max-width: 599px)';
+const mediaQueryList = window.matchMedia(media);
+
+console.log(originalSpeeds);
+
+const mm = gsap.matchMedia();
+
+if (ScrollTrigger.isTouch !== 1) {
+    const scroller = ScrollSmoother.create({
+        wrapper: '.scroll-wrapper',
+        content: '.scroll-content',
+        smooth: 1.5,
+        effects: true,
+    })
+
+    gsap.to('.hero', {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.hero',
+            start: 200,
+            end: 820,
+            scrub: true
+        }
+    })
+
+    const galleryItemsLeft = gsap.utils.toArray('.gallery__side--left .gallery__item');
+
+    galleryItemsLeft.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: -50
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                scrub: true
+            }
+        })
+    })
+
+    const galleryItemsRight = gsap.utils.toArray('.gallery__side--right .gallery__item');
+
+    galleryItemsRight.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: 50
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                scrub: true
+            }
+        })
+    })
+}
